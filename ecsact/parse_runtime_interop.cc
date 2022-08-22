91999cf8fd6002a9f510d6fa886a7232a6430ca7
Ezekiel Warren <zekewarren@gmail.com>
2022-08-22 15:15:25 -0700

    With statement resolve

--- ecsact/parse_runtime_interop.cc
+++ ecsact/parse_runtime_interop.cc
@@ -5,51 +5,210 @@
 #include <string_view>
 #include <vector>
 #include <string>
+#include <string_view>
+#include <cassert>
+#include <optional>
 #include "ecsact/parse.h"
 #include "ecsact/runtime/dynamic.h"
 #include "ecsact/runtime/meta.h"
 
 #include "detail/visit_statement.hh"
 
-static ecsact_component_id ensure_component(ecsact_statement statement) {
-	auto& data = statement.data.component_statement;
-	std::string_view comp_name(
-		data.component_name.data,
-		data.component_name.length
-	);
-
-	std::vector<ecsact_component_id> component_ids;
-	component_ids.resize(ecsact_meta_count_components());
-	ecsact_meta_get_component_ids(
-		component_ids.size(),
-		component_ids.data(),
+std::optional<ecsact_field_id> find_field_by_name
+	( ecsact_composite_id  compo_id
+	, std::string_view     target_field_name
+	)
+{
+	std::vector<ecsact_field_id> field_ids;
+	field_ids.resize(ecsact_meta_count_fields(compo_id));
+	ecsact_meta_get_field_ids(
+		compo_id,
+		static_cast<int32_t>(field_ids.size()),
+		field_ids.data(),
 		nullptr
 	);
-
-	for(auto comp_id : component_ids) {
-		if(comp_name == ecsact_meta_component_name(comp_id)) {
-			return comp_id;
+	for(auto& field_id : field_ids) {
+		std::string field_name = ecsact_meta_field_name(compo_id, field_id);
+		if(field_name == target_field_name) {
+			return field_id;
 		}
 	}
 
-	return ecsact_create_component(
-		data.component_name.data,
-		data.component_name.length
-	);
+	return {};
 }
 
-struct parse_interop_object {
+struct parse_interop_package {
+	ecsact_package_id package_id = (ecsact_package_id)-1;
+	std::string source_file_path;
+
+	std::unordered_map<int32_t, ecsact_package_id> _dependencies;
+
 	// key is statement ID, value is declaration ID
 	std::unordered_map<int32_t, ecsact_component_id> _components;
+	std::unordered_map<int32_t, ecsact_component_id> _system_components;
+	std::unordered_map<std::string, ecsact_component_id> _component_by_name;
+	std::unordered_map<int32_t, ecsact_system_id> _systems;
+
+	std::unordered_map<int32_t, ecsact_system_like_id> _system_likes;
 	std::unordered_map<int32_t, ecsact_composite_id> _composites;
 
-	void component_interop(ecsact_statement statement) {
-		auto comp_id = ensure_component(statement);
-		_components[statement.id] = comp_id;
-		_composites[statement.id] = ecsact_id_cast<ecsact_composite_id>(comp_id);
+	std::unordered_map<int32_t, ecsact_system_like_id> _sys_comp_belong_to;
+	std::unordered_map<int32_t, ecsact_field_id> _field_ids;
+
+	void package_interop
+		( ecsact_statement statement
+		)
+	{
+		auto& data = statement.data.package_statement;
+
+		assert((int)package_id == -1);
+		package_id = ecsact_create_package(
+			data.main,
+			data.package_name.data,
+			data.package_name.length
+		);
 	}
 
-	void field_interop(ecsact_statement context, ecsact_statement statement) {
+	void import_interop
+		( ecsact_statement statement
+		)
+	{
+	}
+
+	void component_interop
+		( ecsact_statement statement
+		)
+	{
+		auto& data = statement.data.component_statement;
+		auto comp_id = ecsact_create_component(
+			package_id,
+			data.component_name.data,
+			data.component_name.length
+		);
+		_components[statement.id] = comp_id;
+		_composites[statement.id] = ecsact_id_cast<ecsact_composite_id>(comp_id);
+
+		auto comp_name = std::string(data.component_name.data, data.component_name.length);
+		_component_by_name[comp_name] = comp_id;
+	}
+
+	ecsact_system_id system_interop
+		( ecsact_statement statement
+		)
+	{
+		auto& data = statement.data.system_statement;
+		auto sys_id = ecsact_create_system(
+			package_id,
+			data.system_name.data,
+			data.system_name.length
+		);
+		auto sys_like_id = ecsact_id_cast<ecsact_system_like_id>(sys_id);
+		_systems[statement.id] = sys_id;
+		_system_likes[statement.id] = sys_like_id;
+
+		return sys_id;
+	}
+
+	void nested_system_interop
+		( ecsact_statement context
+		, ecsact_statement statement
+		)
+	{
+		auto parent_id = _system_likes.at(context.id);
+		auto sys_id = system_interop(statement);
+		ecsact_add_child_system(parent_id, sys_id);
+	}
+
+	void with_interop
+		( ecsact_statement context
+		, ecsact_statement statement
+		)
+	{
+		auto sys_like_id = _sys_comp_belong_to.at(context.id);
+		auto comp_name = std::string(
+			context.data.system_component_statement.component_name.data,
+			context.data.system_component_statement.component_name.length
+		);
+		auto comp_id = _component_by_name.at(comp_name);
+		auto compo_id = ecsact_id_cast<ecsact_composite_id>(comp_id);
+		auto& data = statement.data.system_with_entity_statement;
+
+		std::string field_name(
+			data.with_entity_field_name.data,
+			data.with_entity_field_name.length
+		);
+
+		auto field_id = find_field_by_name(compo_id, field_name);
+		// TODO(zaucy): Report invalid field name error
+		assert(field_id);
+		_field_ids[statement.id] = *field_id;
+	}
+
+	void system_component_interop
+		( ecsact_statement context
+		, ecsact_statement statement
+		)
+	{
+		auto& data = statement.data.system_component_statement;
+		auto component_name = std::string(
+			data.component_name.data,
+			data.component_name.length
+		);
+
+		auto comp_id = _component_by_name.at(component_name);
+		_system_components[statement.id] = comp_id;
+
+		ecsact_system_like_id parent_sys_like_id;
+		if(context.type == ECSACT_STATEMENT_SYSTEM_WITH_ENTITY) {
+			parent_sys_like_id = _sys_comp_belong_to.at(statement.id);
+			ecsact_set_system_association_capability(
+				parent_sys_like_id,
+				_system_components.at(context.id),
+				_field_ids.at(context.id),
+				comp_id,
+				data.capability
+			);
+		} else {
+			if(context.type == ECSACT_STATEMENT_SYSTEM_COMPONENT) {
+				parent_sys_like_id = _sys_comp_belong_to.at(context.id);
+			} else {
+				parent_sys_like_id = _system_likes.at(context.id);
+			}
+			ecsact_set_system_capability(
+				parent_sys_like_id,
+				comp_id,
+				data.capability
+			);
+		}
+
+		_sys_comp_belong_to[statement.id] = parent_sys_like_id;
+
+		// Treating the shorthand with syntax as a faux with statement
+		if(data.with_entity_field_name.length > 0) {
+			ecsact_statement faux_with_context;
+			faux_with_context.id = statement.id;
+			faux_with_context.type = ECSACT_STATEMENT_SYSTEM_COMPONENT;
+			faux_with_context.data.system_component_statement = {
+				.capability = data.capability,
+				.component_name = data.component_name,
+				.with_entity_field_name = {},
+			};
+
+			ecsact_statement faux_with;
+			faux_with.id = -1;
+			faux_with.type = ECSACT_STATEMENT_SYSTEM_WITH_ENTITY;
+			faux_with.data.system_with_entity_statement.with_entity_field_name =
+				data.with_entity_field_name;
+
+			with_interop(faux_with_context, faux_with);
+		}
+	}
+
+	void field_interop
+		( ecsact_statement context
+		, ecsact_statement statement
+		)
+	{
 		auto& composite_id = _composites.at(context.id);
 		auto& data = statement.data.field_statement;
 
@@ -64,24 +223,76 @@ struct parse_interop_object {
 	}
 };
 
+static void destroy_all_packages() {
+	auto packages_size = ecsact_meta_count_packages();
+	std::vector<ecsact_package_id> package_ids;
+	package_ids.resize(packages_size);
+
+	ecsact_meta_get_package_ids(package_ids.size(), package_ids.data(), nullptr);
+
+	for(auto& pkg_id : package_ids) {
+		ecsact_destroy_package(pkg_id);
+	}
+}
+
 void ecsact_parse_runtime_interop
 	( const char**  file_paths
 	, int           file_paths_count
 	)
 {
-	parse_interop_object obj;
+	destroy_all_packages();
+
+	std::vector<parse_interop_package> interop_packages;
+	interop_packages.resize(file_paths_count);
 
 	ecsact_parse_with_cpp_callback(
 		file_paths,
 		file_paths_count,
 		[&](ecsact_parse_callback_params params) {
+			auto& pkg = interop_packages.at(params.source_file_index);
+
 			switch(params.statement->type) {
+				case ECSACT_STATEMENT_NONE:
+				case ECSACT_STATEMENT_UNKNOWN:
+					break;
+				case ECSACT_STATEMENT_PACKAGE:
+					pkg.package_interop(*params.statement);
+					pkg.source_file_path = file_paths[params.source_file_index];
+					break;
+				case ECSACT_STATEMENT_IMPORT:
+					pkg.import_interop(*params.statement);
+					break;
 				case ECSACT_STATEMENT_COMPONENT:
-					obj.component_interop(*params.statement);
+					pkg.component_interop(*params.statement);
 					break;
 				case ECSACT_STATEMENT_BUILTIN_TYPE_FIELD:
-					obj.field_interop(*params.context_statement, *params.statement);
+					pkg.field_interop(*params.context_statement, *params.statement);
 					break;
+				case ECSACT_STATEMENT_SYSTEM:
+					if(params.context_statement) {
+						pkg.nested_system_interop(
+							*params.context_statement,
+							*params.statement
+						);
+					} else {
+						pkg.system_interop(*params.statement);
+					}
+					break;
+				case ECSACT_STATEMENT_SYSTEM_COMPONENT:
+					pkg.system_component_interop(
+						*params.context_statement,
+						*params.statement
+					);
+					break;
+				case ECSACT_STATEMENT_SYSTEM_WITH_ENTITY:
+					pkg.with_interop(
+						*params.context_statement,
+						*params.statement
+					);
+					break;
+				// case ECSACT_STATEMENT_ACTION:
+				// 	pkg.action_interop(*params.statement);
+				// 	break;
 			}
 
 			return ECSACT_PARSE_CALLBACK_CONTINUE;
--- test/test.cc
+++ test/test.cc
@@ -2,11 +2,46 @@
 
 #include <array>
 #include <filesystem>
+#include <unordered_map>
+#include <functional>
 #include "ecsact/parse_runtime_interop.h"
 #include "ecsact/runtime/meta.h"
 #include "bazel_sundry/runfiles.hh"
 
 namespace fs = std::filesystem;
+
+template<typename... CallbackPairs>
+void for_each_field
+	( ecsact_composite_id  compo_id
+	, CallbackPairs&&...   callback_pairs
+	)
+{
+	auto field_count = ecsact_meta_count_fields(compo_id);
+	std::vector<ecsact_field_id> field_ids;
+	field_ids.resize(field_count);
+	ecsact_meta_get_field_ids(
+		compo_id,
+		field_ids.size(),
+		field_ids.data(),
+		&field_count
+	);
+	ASSERT_EQ(field_count, field_ids.size());
+
+	using pairs_t = std::unordered_map
+		< std::string
+		, std::function<void(ecsact_field_id)>
+		>;
+	pairs_t pairs{std::forward<CallbackPairs>(callback_pairs)...};
+
+	for(auto& field_id : field_ids) {
+		std::string field_name = ecsact_meta_field_name(compo_id, field_id);
+		ASSERT_TRUE(pairs.contains(field_name))
+			<< "Missing test body for field '" << field_name << "'";
+		pairs.at(field_name)(field_id);
+	}
+
+	ASSERT_EQ(field_ids.size(), pairs.size());
+}
 
 TEST(EcsactParseRuntimeInterop, Simple) {
 	auto runfiles = bazel_sundry::CreateDefaultRunfiles();
@@ -20,30 +55,104 @@ TEST(EcsactParseRuntimeInterop, Simple) {
 
 	auto files = std::array{test_ecsact.c_str()};
 
-	// Run the interop
-	ecsact_parse_runtime_interop(files.data(), files.size());
+	auto test_interop = [&] {
+		ecsact_parse_runtime_interop(files.data(), files.size());
 
-	// Our test file only contains 1 component
-	ASSERT_EQ(ecsact_meta_count_components(), 1);
+		// Only 1 package
+		ASSERT_EQ(ecsact_meta_count_packages(), 1);
+		auto package_id = (ecsact_package_id)-1;
+		ecsact_meta_get_package_ids(1, &package_id, nullptr);
+		ASSERT_NE(package_id, (ecsact_package_id)-1);
 
-	auto comp_id = ecsact_invalid_component_id;
-	ecsact_meta_get_component_ids(1, &comp_id, nullptr);
-	ASSERT_NE(comp_id, ecsact_invalid_component_id);
-	auto compo_id = ecsact_id_cast<ecsact_composite_id>(comp_id);
+		ASSERT_STREQ(ecsact_meta_package_name(package_id), "test");
 
-	ASSERT_STREQ("TestComponent", ecsact_meta_component_name(comp_id));
-	ASSERT_EQ(ecsact_meta_count_fields(compo_id), 1);
+		// Our test file only contains 1 component
+		ASSERT_EQ(ecsact_meta_count_components(), 1);
 
-	ecsact_field_id field_id = (ecsact_field_id)-1;
-	ecsact_meta_get_field_ids(compo_id, 1, &field_id, nullptr);
-	ASSERT_NE((int)field_id, -1);
+		auto comp_id = ecsact_invalid_component_id;
+		ecsact_meta_get_component_ids(1, &comp_id, nullptr);
+		ASSERT_NE(comp_id, ecsact_invalid_component_id);
+		auto compo_id = ecsact_id_cast<ecsact_composite_id>(comp_id);
 
-	ASSERT_STREQ(ecsact_meta_field_name(compo_id, field_id), "num");
-	ASSERT_EQ(ecsact_meta_field_type(compo_id, field_id).type, ECSACT_I32);
-	ASSERT_EQ(ecsact_meta_field_type(compo_id, field_id).length, 1);
+		ASSERT_STREQ("TestComponent", ecsact_meta_component_name(comp_id));
+		for_each_field(
+			compo_id,
+			std::make_pair("num", [&](ecsact_field_id field_id) {
+				auto type = ecsact_meta_field_type(compo_id, field_id);
+				ASSERT_EQ(type.type, ECSACT_I32);
+				ASSERT_EQ(type.length, 1);
+			}),
+			std::make_pair("test_entity", [&](ecsact_field_id field_id) {
+				auto type = ecsact_meta_field_type(compo_id, field_id);
+				ASSERT_EQ(type.type, ECSACT_ENTITY_TYPE);
+				ASSERT_EQ(type.length, 1);
+			})
+		);
+		
+		// Here we're creating a function for each system in our test file to make
+		// sure we have the expected capabilities
+		using test_system_fns_t = std::unordered_map
+			< std::string
+			, std::function<void(ecsact_system_id)>
+			>;
+		test_system_fns_t test_system_fns{
+			{"TestSystem", [&](ecsact_system_id sys_id) {
+				auto sys_like_id = ecsact_id_cast<ecsact_system_like_id>(sys_id);
 
-	// Running it again should result in there still being only 1 component
-	// because the component name hasn't changed.
-	ecsact_parse_runtime_interop(files.data(), files.size());
-	ASSERT_EQ(ecsact_meta_count_components(), 1);
+				ASSERT_EQ(ecsact_meta_system_capabilities_count(sys_like_id), 1);
+
+				std::array sys_cap_comp_ids{ecsact_invalid_component_id};
+				std::array sys_caps{ecsact_system_capability{}};
+				ecsact_meta_system_capabilities(
+					sys_like_id,
+					1,
+					sys_cap_comp_ids.data(),
+					sys_caps.data(),
+					nullptr
+				);
+
+				ASSERT_EQ(sys_cap_comp_ids[0], comp_id);
+				ASSERT_EQ(sys_caps[0], ECSACT_SYS_CAP_READWRITE);
+			}},
+			{"TestNestedSystem", [&](ecsact_system_id sys_id) {
+				auto sys_like_id = ecsact_id_cast<ecsact_system_like_id>(sys_id);
+
+				ASSERT_EQ(ecsact_meta_system_capabilities_count(sys_like_id), 1);
+
+				std::array sys_cap_comp_ids{ecsact_invalid_component_id};
+				std::array sys_caps{ecsact_system_capability{}};
+				ecsact_meta_system_capabilities(
+					sys_like_id,
+					1,
+					sys_cap_comp_ids.data(),
+					sys_caps.data(),
+					nullptr
+				);
+
+				ASSERT_EQ(sys_cap_comp_ids[0], comp_id);
+				ASSERT_EQ(sys_caps[0], ECSACT_SYS_CAP_READWRITE);
+			}},
+		};
+
+		ASSERT_EQ(ecsact_meta_count_systems(), test_system_fns.size());
+		std::vector<ecsact_system_id> sys_ids;
+		sys_ids.resize(test_system_fns.size());
+
+		int32_t out_count = 0;
+		ecsact_meta_get_system_ids(sys_ids.size(), sys_ids.data(), &out_count);
+		ASSERT_EQ(sys_ids.size(), out_count);
+
+		for(auto sys_id : sys_ids) {
+			std::string sys_name = ecsact_meta_system_name(sys_id);
+			ASSERT_TRUE(test_system_fns.contains(sys_name))
+				<< "No test function for the '"
+				<< sys_name << "' system";
+			test_system_fns.at(sys_name)(sys_id);
+		}
+	};
+
+	test_interop();
+	// Running the interop a second time should be no different since all defs are
+	// removed.
+	test_interop();
 }
