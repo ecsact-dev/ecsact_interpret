efe15b4987bddb1726b34f1f71248e19f0bf5aa7
Ezekiel Warren <zekewarren@gmail.com>
2023-03-07 13:41:16 -0800

    formatting

--- test/errors/no_package_statement_first.cc
+++ test/errors/no_package_statement_first.cc
@@ -4,7 +4,8 @@
 #include "test/test_lib.hh"
 
 TEST(NoPackageStatementFirst, NoPackageStatementFirst) {
-	auto errs = ecsact_interpret_test_files({"errors/no_package_statement_first.ecsact"});
+	auto errs =
+		ecsact_interpret_test_files({"errors/no_package_statement_first.ecsact"});
 	ASSERT_EQ(errs.size(), 1);
 	ASSERT_EQ(errs[0].eval_error, ECSACT_EVAL_ERR_EXPECTED_PACKAGE_STATEMENT);
 }
